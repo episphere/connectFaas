bff3b259e4fc1478d22479046d9e29001dd50269
bhaumik55231 <bhaumik55231@gmail.com>
2020-01-16 16:43:26 -0500

    Mocha test cases

---
+++ test/test.js
@@ -0,0 +1,80 @@
+const assert = require('assert');
+const Supertest = require('supertest');
+const supertest = Supertest('http://localhost:8010/nih-nci-dceg-episphere-dev/us-central1');
+const bearerToken = 'Bearer GFDS365Hjweuy5326qwkjgddghDASHDgh';
+
+describe('getParticipantToken API: -', () => {
+    const endPoint = '/getParticipantToken';
+    it(`${endPoint}: should return 405`, async () => {
+        await supertest
+        .get(endPoint)
+        .expect(405);
+    });
+    it(`${endPoint}: should return 401`, async () => {
+        await supertest
+        .post(endPoint)
+        .expect(401);
+    });
+    it(`${endPoint}: should return 400`, async () => {
+        await supertest
+        .post(endPoint)
+        .set('Authorization', bearerToken)
+        .set('Content-Type', 'application/json')
+        .expect(400);
+    });
+    const requestArray = [{"studyId": 'NCI-ajsr76'}, {"studyId": 'NCI-ajwdar76'}];
+    it(`${endPoint}: should return 200`, async () => {
+        await supertest
+        .post(endPoint)
+        .set('Authorization', bearerToken)
+        .set('Content-Type', 'application/json')
+        .send({"data": requestArray})
+        .expect(200)
+        .expect(response => {
+            assert.equal(Array.isArray(response.body.data), true);
+            assert.equal(response.body.data.length, requestArray.length);
+        });
+    });
+});
+
+describe('getParticipants API: -', () => {
+    const endPoint = '/getParticipants';
+    it(`${endPoint}: should return 405`, async () => {
+        await supertest
+        .post(endPoint)
+        .expect(405);
+    });
+    it(`${endPoint}: should return 401`, async () => {
+        await supertest
+        .get(endPoint)
+        .expect(401);
+    });
+    it(`${endPoint}: should return 404`, async () => {
+        await supertest
+        .get(endPoint)
+        .set('Authorization', bearerToken)
+        .set('Content-Type', 'application/json')
+        .expect(404);
+    });
+    it(`${endPoint}: should return 200`, async () => {
+        await supertest
+        .get(endPoint+'?type=all')
+        .set('Authorization', bearerToken)
+        .set('Content-Type', 'application/json')
+        .expect(200);
+    });
+    it(`${endPoint}: should return 200`, async () => {
+        await supertest
+        .get(endPoint+'?type=verified')
+        .set('Authorization', bearerToken)
+        .set('Content-Type', 'application/json')
+        .expect(200);
+    });
+    it(`${endPoint}: should return 200`, async () => {
+        await supertest
+        .get(endPoint+'?type=notverified')
+        .set('Authorization', bearerToken)
+        .set('Content-Type', 'application/json')
+        .expect(200);
+    });
+});
