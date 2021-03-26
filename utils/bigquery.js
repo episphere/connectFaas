f2b3443f0c9cbd271b00a97b38645d8cdb242120
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2021-03-26 12:22:47 -0400

    Addressing GIT commits

--- utils/bigquery.js
+++ utils/bigquery.js
@@ -2,7 +2,7 @@ const {BigQuery} = require('@google-cloud/bigquery');
 const bigquery = new BigQuery();
 
 
-const getTable = async (tableName, isParent, siteCode, status, recruit) => {
+const getTable = async (tableName, isParent, siteCode) => {
     const dataset = bigquery.dataset('stats');
     const [tableData] = await dataset.table(tableName).getRows();
     let data = '';
@@ -13,18 +13,6 @@ const getTable = async (tableName, isParent, siteCode, status, recruit) => {
     }
     else {
         data = tableData.filter(dt => dt.siteCode === siteCode)
-    }
-
-    if (status === 'participantsRecruitsCount') {
-        return data;
-    }
-
-    if (status === 'participantsWorkflow'){
-        return data;
-    }
-
-    if (status === 'participantsVerification') {
-        return data;
     }
   
     return data;
