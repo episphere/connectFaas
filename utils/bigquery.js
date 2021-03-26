780dec35e2780a29158b7ff67cfa7bddb902f969
bhaumik55231 <bhaumik55231@gmail.com>
2021-03-26 12:29:05 -0400

    minor update

--- utils/bigquery.js
+++ utils/bigquery.js
@@ -7,13 +7,8 @@ const getTable = async (tableName, isParent, siteCode) => {
     const [tableData] = await dataset.table(tableName).getRows();
     let data = '';
 
-    if(isParent){
-        
-        data = tableData.filter(dt => siteCode.indexOf(dt.siteCode) !== -1)
-    }
-    else {
-        data = tableData.filter(dt => dt.siteCode === siteCode)
-    }
+    if(isParent) data = tableData.filter(dt => siteCode.indexOf(dt.siteCode) !== -1)
+    else data = tableData.filter(dt => dt.siteCode === siteCode)
   
     return data;
 }
