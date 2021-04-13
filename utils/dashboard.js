a7ce2ba3b975e65b76d0dd349ea80812f8ee3be0
bhaumik55231 <bhaumik55231@gmail.com>
2021-04-13 13:54:05 -0400

    minor update

--- utils/dashboard.js
+++ utils/dashboard.js
@@ -10,27 +10,33 @@ const dashboard = async (req, res) => {
     if(!siteDetails) return res.status(401).json(getResponseJSON('Authorization failed!', 401));
     const { isParentEntity } = require('./shared');
     const authObj = await isParentEntity(siteDetails);
-    
+    const isParent = authObj.isParent;
+    const siteCodes = authObj.siteCodes;
     const query = req.query;
     if(!query.api) return res.status(400).json(getResponseJSON('Bad request!', 400));
     const api = query.api;
-    switch (api) {
-        case 'validateSiteUsers': 
-            break;
-        case 'getParticipants':
-            break;
-        case 'identifyParticipant':
-            break;
-        case 'submitParticipantsData':
-            break;
-        case 'updateParticipantData':
-            break;
-        case 'stats':
-            const { stats } = require('./stats');
-            return await stats(req, res, authObj);
-        default: 
-            return res.status(404).json(getResponseJSON('API not found!', 404));
+
+    if(api === 'validateSiteUsers') {
+
     }
+    else if (api === 'getParticipants') {
+
+    }
+    else if (api === 'identifyParticipant') {
+
+    }
+    else if (api === 'submitParticipantsData') {
+
+    }
+    else if (api === 'updateParticipantData' && isParent === false) {
+        const { updateParticipantData } = require('./sites');
+        return await updateParticipantData(req, res, siteCodes);
+    }
+    else if (api === 'stats') {
+        const { stats } = require('./stats');
+        return await stats(req, res, authObj);
+    }
+    else return res.status(404).json(getResponseJSON('API not found!', 404));
 }
 
 module.exports = {
