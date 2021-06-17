2bcc4ce14468863b7fa4c043e708b05c3a73754d
bhaumik55231 <bhaumik55231@gmail.com>
2021-06-17 12:55:25 -0400

    updating projectId and bucket name

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
-const gcsBucket = 'connect_firestore_dev_backup';
-const projectId = 'nih-nci-dceg-connect-dev';
+const gcsBucket = 'myconnect_firestore_backup_stage';
+const projectId = 'nih-nci-dceg-connect-stg-5519';
 
 module.exports = {
     gcsBucket,
