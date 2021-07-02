7107bbfe6ff5191b0d4a1f0819526bd6637b8938
bhaumik55231 <bhaumik55231@gmail.com>
2021-07-02 16:43:34 -0400

    BUG FIX

--- utils/shared.js
+++ utils/shared.js
@@ -12,7 +12,6 @@ const setHeadersDomainRestricted = (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers','Accept,Content-Type,Content-Length,Accept-Encoding,X-CSRF-Token,Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-   }
 }
 
 const generateConnectID = () => {
