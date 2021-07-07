4981a9ccc3535856060ad5b4dd2a6aaa5c51a986
bhaumik55231 <bhaumik55231@gmail.com>
2021-07-07 11:16:45 -0400

    Return empty array if table not found

--- utils/bigquery.js
+++ utils/bigquery.js
@@ -3,14 +3,19 @@ const bigquery = new BigQuery();
 
 
 const getTable = async (tableName, isParent, siteCode) => {
-    const dataset = bigquery.dataset('stats');
-    const [tableData] = await dataset.table(tableName).getRows();
-    let data = '';
+    try {
+        const dataset = bigquery.dataset('stats');
+        const [tableData] = await dataset.table(tableName).getRows();
+        let data = '';
 
-    if(isParent) data = tableData.filter(dt => siteCode.indexOf(dt.siteCode) !== -1)
-    else data = tableData.filter(dt => dt.siteCode === siteCode)
-  
-    return data;
+        if(isParent) data = tableData.filter(dt => siteCode.indexOf(dt.siteCode) !== -1)
+        else data = tableData.filter(dt => dt.siteCode === siteCode)
+    
+        return data;
+    } catch (error) {
+        if(/Not found: Table/i.test(error.message)) return [];
+        else console.error(error)
+    }
 }
 
 module.exports = {
