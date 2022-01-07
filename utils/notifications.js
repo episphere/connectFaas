03f972ffd8d0f7f016d0bf5284a2cf88227ac176
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2022-01-07 11:30:24 -0500

    added conditon for timetap connectID

--- utils/notifications.js
+++ utils/notifications.js
@@ -1,4 +1,5 @@
 const { getResponseJSON, setHeadersDomainRestricted, setHeaders, logIPAdddress } = require('./shared');
+const { fieldMapping } = require('./fieldMapping.js')
 
 const subscribeToNotification = async (req, res) => {
     setHeadersDomainRestricted(req, res);
@@ -186,7 +187,7 @@ const notificationHandler = async (message, context) => {
                 d.setHours(d.getHours() + hour);
                 d.setMinutes(d.getMinutes() + minute);
                 let body = html.replace('<firstName>', preferredNameField && participant[preferredNameField] ? participant[preferredNameField] : participant[firstNameField]);
-                body = html.replace('<Connect_ID>', participant['Connect_ID']);
+                (participant[fieldMapping.site] === fieldMapping.UofChiM && participant[fieldMapping.verifiedFlag] === fieldMapping.verified) ? (body += html.replace('<Connect_ID>', participant['Connect_ID'])) : ``
                 let reminder = {
                     notificationSpecificationsID,
                     id: uuid(),
