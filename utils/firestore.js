4d96e620aecb0d60e2342cd4cb34c030d646f2de
Bhaumik Patel <bhaumik55231@gmail.com>
2022-02-10 11:04:06 -0500

    bug fix, removing update to the specimen link

--- utils/firestore.js
+++ utils/firestore.js
@@ -916,11 +916,7 @@ const searchShipments = async (siteAcronym) => {
 
 const specimenExists = async (id, data) => {
     const snapshot = await db.collection('biospecimen').where('820476880', '==', id).get();
-    if(snapshot.size === 1) {
-        const docId = snapshot.docs[0].id;
-        await db.collection('biospecimen').doc(docId).update(data);
-        return true;
-    }
+    if(snapshot.size === 1) return true;
     else return false;
 }
 
