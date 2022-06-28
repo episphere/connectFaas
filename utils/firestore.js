db316f6d4faa0b1d6a4098d5f2ecb6e807143d5b
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2022-06-27 22:59:34 -0400

    added revamped code

--- utils/firestore.js
+++ utils/firestore.js
@@ -1635,33 +1635,31 @@ const getQueryBsiData = async (query) => {
     try {
         let storeResults = []
         let holdBiospecimenMatches = []
-        const snapshot = await db.collection("biospecimen").where('926457119', '==', query).get();
-        let tubeConceptIds = Object.values(collectionIdConversion); // grab tube id
-        snapshot.docs.map(doc => {
-            holdBiospecimenMatches.push(doc.data()) // push query results to holdBiospecimenMatches array
-        })
-
-
-        holdBiospecimenMatches.forEach( i => { // if query results matches/exists in tubeconcepts ids then add them to below object
-            tubeConceptIds.forEach( id => {
-                if (id in i) {
-                    let collectionIdInfo = {}
-                    collectionIdInfo['825582494'] = i[id]['825582494']
-                    collectionIdInfo['926457119'] = i['926457119']
-                    collectionIdInfo['678166505'] = i['678166505']
-                    collectionIdInfo['Connect_ID'] = i['Connect_ID']
-                    // collectionIdInfo['789843387'] = i['789843387']
-                    collectionIdInfo['827220437'] = i['827220437']
-                    collectionIdInfo['951355211'] = i['951355211']
-                    collectionIdInfo['650516960'] = i['650516960']
-                    collectionIdInfo['762124027'] = i[id]['762124027'] === undefined ? ``  : i[id]['762124027']
-                    collectionIdInfo['982885431'] = i[id]['248868659'] === undefined ? `` : i[id]['248868659']['982885431']
-        
-                    storeResults.push(collectionIdInfo)
-                }
+        let tubeConceptIds = ``; // grab tube id
+        //let id = '838567176'
+       for(let id of Object.values(collectionIdConversion)) {
+            const snapshot = await db.collection("biospecimen").where(`${id}.926457119`, '==', query).get();
+            snapshot.docs.map(doc => {
+                holdBiospecimenMatches.push(doc.data()) // push query results to holdBiospecimenMatches array
             })
-
-        })
+            console.log('sr', holdBiospecimenMatches)
+            holdBiospecimenMatches.forEach( i => { // if query results matches/exists in tubeconcepts ids then add them to below object
+                let collectionIdInfo = {}
+                collectionIdInfo['825582494'] = i[id]['825582494'] === undefined ? `` : i[id]['825582494']
+                collectionIdInfo['926457119'] = i['926457119']
+                collectionIdInfo['678166505'] = i['678166505']
+                collectionIdInfo['Connect_ID'] = i['Connect_ID']
+                // collectionIdInfo['789843387'] = i['789843387']
+                collectionIdInfo['827220437'] = i['827220437']
+                collectionIdInfo['951355211'] = i['951355211']
+                collectionIdInfo['650516960'] = i['650516960']
+                collectionIdInfo['762124027'] = i[id]['762124027'] === undefined ? ``  : i[id]['762124027']
+                collectionIdInfo['982885431'] = i[id]['248868659'] === undefined ? `` : i[id]['248868659']['982885431']
+                storeResults.push(collectionIdInfo)
+            }) // asha profind
+            
+    }
+    console.log('sr', storeResults)
         return storeResults
     }
     catch(error){
