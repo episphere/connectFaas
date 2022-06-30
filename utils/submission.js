990fae6bd115aaaf744b01caab5295b8467f2ea4
anthonypetersen <tonypetersen@hotmail.com>
2022-06-30 12:00:16 -0500

    console logs

--- utils/submission.js
+++ utils/submission.js
@@ -299,6 +299,7 @@ const identifyParticipant = async (req, res, site) => {
 
 const getUserProfile = async (req, res, uid) => {
 
+    console.log("Res1: " + res);
     if(req.method !== 'GET') {
         return res.status(405).json(getResponseJSON('Only GET requests are accepted!', 405));
     }
@@ -315,6 +316,7 @@ const getUserProfile = async (req, res, uid) => {
     }
 
     if(response){
+        console.log("Res2: " + res);
         let defaultConcepts = checkDefaultFlags(response[0]);
         console.log("Missing Defaults: " + Object.entries(defaultConcepts).length);
         console.log(defaultConcepts);
@@ -323,7 +325,7 @@ const getUserProfile = async (req, res, uid) => {
         if(Object.entries(defaultConcepts).length != 0) {
             let submitRes = res;
             response = await submit(submitRes, defaultConcepts, uid);
-
+            console.log("Res3: " + res);
             if(response instanceof Error){
                 return res.status(500).json(getResponseJSON(response.message, 500));
             }
@@ -334,7 +336,7 @@ const getUserProfile = async (req, res, uid) => {
                 return res.status(500).json(getResponseJSON(response.message, 500));
             }
         }
-
+        console.log("Res4: " + res);
         return res.status(200).json({data: response[0], code:200});
     }
 }
