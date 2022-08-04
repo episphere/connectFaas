61f3e31f8534d67d89a6c14cc03ead456f080883
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2022-08-04 09:39:08 -0400

    added newsletter condt. to skip html conversion

--- utils/notifications.js
+++ utils/notifications.js
@@ -108,6 +108,7 @@ const sendEmail = async (emailTo, messageSubject, html, cc) => {
 const notificationHandler = async (message) => {
     const publishedMessage = message.data ? Buffer.from(message.data, 'base64').toString().trim() : null;
     const splitCharacters = '@#$'
+    const html = ``
     
     if(!/@#\$/.test(publishedMessage)) {
         const {PubSub} = require('@google-cloud/pubsub');
@@ -154,10 +155,14 @@ const notificationHandler = async (message) => {
         const day = obj.time.day;
         const hour = obj.time.hour;
         const minute = obj.time.minute;
-
-        const showdown  = require('showdown');
-        const converter = new showdown.Converter();
-        const html = converter.makeHtml(messageBody);
+        if(obj.category === `newsletterTest`) {
+            html = messageBody
+        }
+        else {
+            const showdown  = require('showdown');
+            const converter = new showdown.Converter();
+            html = converter.makeHtml(messageBody);
+        }
         const uuid = require('uuid');
 
         console.log("Conditions: " + JSON.stringify(conditions));
