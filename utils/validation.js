c90f926a67409017e3307d2641e71bc9219136b2
anthonypetersen <tonypetersen@hotmail.com>
2022-12-19 16:17:07 -0600

    adding null check

--- utils/validation.js
+++ utils/validation.js
@@ -234,19 +234,21 @@ const checkDerivedVariables = async (token, siteCode) => {
             }    
             else {
                 
-                const baselineResearchCollections = collections.filter(collection => collection['331584571'] === 266600170 && collection['650516960'] === 534621077);
+                if(collections) {
+                    const baselineResearchCollections = collections.filter(collection => collection['331584571'] === 266600170 && collection['650516960'] === 534621077);
                 
-                if(baselineResearchCollections.length != 0) {
-                    baselineResearchCollections.forEach(collection => {
-                        
-                        const researchBloodTubes = ['299553921', '703954371', '838567176', '454453939', '652357376'];
-                        
-                        researchBloodTubes.forEach(tube => {
-                            if(collection[tube] && collection[tube][883732523] && collection[tube][883732523] != 681745422) {
-                                incentiveEligible = true;
-                            }
+                    if(baselineResearchCollections.length != 0) {
+                        baselineResearchCollections.forEach(collection => {
+                            
+                            const researchBloodTubes = ['299553921', '703954371', '838567176', '454453939', '652357376'];
+                            
+                            researchBloodTubes.forEach(tube => {
+                                if(collection[tube] && collection[tube][883732523] && collection[tube][883732523] != 681745422) {
+                                    incentiveEligible = true;
+                                }
+                            });
                         });
-                    });
+                    }
                 }
             }
         }
