816e63c8115e43e108d4ef7b4091b07d8ca9cc9a
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2023-04-21 15:49:30 -0400

    removed comment

--- utils/sites.js
+++ utils/sites.js
@@ -152,7 +152,7 @@ const submitParticipantsData = async (req, res, site) => {
         responseArray.push({'Success': {'Token': participantToken, 'Errors': 'None'}});
     }
 
-    return res.status(error ? 206 : 200).json({code: error ? 206 : 200, results: responseArray}); // 206 one or more pt error
+    return res.status(error ? 206 : 200).json({code: error ? 206 : 200, results: responseArray});
 }
 
 const siteNotificationsHandler = async (Connect_ID, concept, siteCode, obj) => {
