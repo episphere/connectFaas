c9b00eb0d8ec0648d455a56695f2d35fec082542
Joe Armani <93854858+JoeArmani@users.noreply.github.com>
2023-08-16 09:56:35 -0400

    update per Jessica's comments

--- utils/biospecimen.js
+++ utils/biospecimen.js
@@ -275,7 +275,7 @@ const biospecimenAPIs = async (req, res) => {
         }
         return res.status(200).json({message: 'Success!', code:200})
     }
-    else if(api == 'addBoxAndUpdateSiteDetails'){
+    else if(api === 'addBoxAndUpdateSiteDetails'){
         try {
             if(req.method !== 'POST') {
                 return res.status(405).json(getResponseJSON('Only POST requests are accepted!', 405));
