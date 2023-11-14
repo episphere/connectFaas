3908777a76d0af16e8455d11da1a74d5e32e3d5e
Warren Lu <warren.lu6@gmail.com>
2023-11-14 14:12:40 -0500

    sync kitAsselbly data and cleanup

--- utils/events.js
+++ utils/events.js
@@ -2,95 +2,74 @@ const firestore = require('@google-cloud/firestore');
 const {BigQuery} = require('@google-cloud/bigquery');
 const {Storage} = require('@google-cloud/storage');
 
-const collectionNameList = ['participants','biospecimen', 'boxes', 'module1_v1', 'module1_v2', 'module2_v1', 'module2_v2', 'module3_v1', 'module4_v1', 'bioSurvey_v1', 'menstrualSurvey_v1', 'clinicalBioSurvey_v1','covid19Survey_v1'];
-
-const importToBigQuery = async (event, context) => {
-    const gcsEvent = event;
-    let tableName = ''
-    if (gcsEvent.name.indexOf('.export_metadata') === -1) return true;
-    for (const collectionName  of collectionNameList) {
-        if (gcsEvent.name.includes(collectionName)) {
-            tableName = collectionName;
-            break;
-        }
-    }
-
-    console.log(`Processing file: ${gcsEvent.name}`);
-    const gcsBucket = process.env.GCLOUD_BUCKET;
-    const url = `gs://${gcsBucket}/${gcsEvent.name}`;
-    console.log(url)
-    const storage = new Storage();
-    const bigquery = new BigQuery();
-  
-    const datasetId = 'Connect';
-    const tableId = tableName;
-  
-    const metadata = {
-      sourceFormat: 'DATASTORE_BACKUP',
-      // projectionFields: ['371067537', 'pin', '494982282', '564964481', '142654897', 'Module1', 'query'],
-      createDisposition: 'CREATE_IF_NEEDED',
-      writeDisposition: 'WRITE_TRUNCATE',
-      location: 'US',
-    };
-  
-    const [job] = await bigquery
-        .dataset(datasetId)
-        .table(tableId)
-        .load(storage.bucket(gcsBucket).file(gcsEvent.name), metadata);
-  
-    // load() waits for the job to finish
-    console.log(`Load job ${job.id} completed.`);
-  
-    // Check the job's status for errors
-    const errors = job.status.errors;
-    if (errors && errors.length > 0) {
-        throw errors;
-    }
-};
+const collectionNameArray = ['participants','biospecimen', 'boxes', 'module1_v1', 'module1_v2', 'module2_v1', 'module2_v2', 'module3_v1', 'module4_v1', 'bioSurvey_v1', 'menstrualSurvey_v1', 'clinicalBioSurvey_v1','covid19Survey_v1', 'kitAssembly'];
 
 const firestoreExport = async (event, context) => {
-    const client = new firestore.v1.FirestoreAdminClient();
-    const gcsBucket = process.env.GCLOUD_BUCKET;
-    console.log(gcsBucket)
-    const bucket = `gs://${gcsBucket}`;
-    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
-    console.log(projectId)
-    const databaseName = client.databasePath(projectId, '(default)');
+  await exportCollectionsToBucket(collectionNameArray);
+};
 
-    await client.exportDocuments({
-        name: databaseName,
-        outputUriPrefix: bucket,
-        collectionIds: collectionNameList,
-    });
-
-    return true;
+const importToBigQuery = async (gcsEvent, context) => {
+  await importCollectionsToBigQuery(gcsEvent, collectionNameArray);
 };
 
 const exportNotificationsToBucket = async (event, context) => {
+  await exportCollectionsToBucket(["notifications"]);
+};
+
+const importNotificationsToBigquery = async (gcsEvent, context) => {
+  await importCollectionsToBigQuery(gcsEvent, ["notifications"]);
+};
+
+/**
+ * Export collections from Firestore to Bucket
+ * @param {string[]} collectionNameArray Array of collection names
+ */
+async function exportCollectionsToBucket(collectionNameArray) {
+  if (collectionNameArray.length === 0) return;
+
   const client = new firestore.v1.FirestoreAdminClient();
   const gcsBucket = process.env.GCLOUD_BUCKET;
   const bucket = `gs://${gcsBucket}`;
   const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
   const databaseName = client.databasePath(projectId, "(default)");
 
-  await client.exportDocuments({
-    name: databaseName,
-    outputUriPrefix: bucket,
-    collectionIds: ["notifications"],
-  });
-  console.log(`Exported notifications to bucket ${gcsBucket}`);
-};
+  try {
+    await client.exportDocuments({
+      name: databaseName,
+      outputUriPrefix: bucket,
+      collectionIds: collectionNameArray,
+    });
 
-const importNotificationsToBigquery = async (event, context) => {
-  const gcsEvent = event;
-  if (!gcsEvent.name.includes(".export_metadata") || !gcsEvent.name.includes("notifications")) return;
-  const gcsBucket = process.env.GCLOUD_BUCKET;
-  const datasetId = "Connect";
-  let tableId = "notifications";
+    console.log(`Exported ${collectionNameArray.length > 1 ? "collections" : "collection"} ${collectionNameArray.join(", ")} to bucket ${bucket}.`);
+  } catch (error) {
+    console.error(`Error occurred when exporting to bucket ${bucket}.`, error);
+  }
+
+}
+
+/**
+ * Import collections from Bucket to BigQuery
+ * @param {Event} gcsEvent 
+ * @param {string[]} collectionNameArray Array of collection names
+ */
+async function importCollectionsToBigQuery(gcsEvent, collectionNameArray) {
+  if (!gcsEvent.name.includes(".export_metadata")) return;
+
+  let tableName = "";
+  for (const collectionName of collectionNameArray) {
+    if (gcsEvent.name.includes(collectionName)) {
+      tableName = collectionName;
+      break;
+    }
+  }
+
+  if (tableName === "") return;
+
   console.log(`Processing file: ${gcsEvent.name}`);
-
+  const gcsBucket = process.env.GCLOUD_BUCKET;
   const storage = new Storage();
   const bigquery = new BigQuery();
+  const datasetName = "Connect";
   const metadata = {
     sourceFormat: "DATASTORE_BACKUP",
     createDisposition: "CREATE_IF_NEEDED",
@@ -98,23 +77,25 @@ const importNotificationsToBigquery = async (event, context) => {
     location: "US",
   };
 
-  const [job] = await bigquery
-    .dataset(datasetId)
-    .table(tableId)
-    .load(storage.bucket(gcsBucket).file(gcsEvent.name), metadata);
+  try {
+    const [job] = await bigquery
+      .dataset(datasetName)
+      .table(tableName)
+      .load(storage.bucket(gcsBucket).file(gcsEvent.name), metadata);
 
-  console.log("Imported notifications to BigQuery.");
+    if (job.status.errorResult) {
+      console.error(`Failed to import '${tableName}' to BigQuery.`, job.status.errorResult);
+    }
 
-  // Check the job's status for errors
-  const errors = job.status.errors;
-  if (errors && errors.length > 0) {
-    throw errors;
+    console.log(`Imported '${tableName}' to BigQuery.`);
+  } catch (err) {
+    console.error(`Error occured when importing to BigQuery:`, err);
   }
-};
+}
 
 module.exports = {
-    importToBigQuery,
-    firestoreExport,
-    exportNotificationsToBucket,
-    importNotificationsToBigquery,
+  importToBigQuery,
+  firestoreExport,
+  exportNotificationsToBucket,
+  importNotificationsToBigquery,
 };
